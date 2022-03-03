6d1d8af7835109ede7ae376bc010d94fecd70054
NurCahyoPrasetiyo <nurcahyoprasetiyo4@gmail.com>
2022-03-03 20:07:34 +0700

    add new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [limitPlayer, setLimitPlayer] = useState(false);
+  const [listPlayers] = useState([]);
+  const [name, setName] = useState("");
+
+  const onChangeform = (data) => {
+    setName(data);
+  };
+
+  const onSubmitPlayers = () => {
+    if (listPlayers.length < 4) {
+      const dataNewPlayer = {
+        name: name,
+        score: 0,
+      };
+
+      listPlayers.push(dataNewPlayer);
+      setName("");
+    } else {
+      alert("DEK MAEN REMI ATAU ARISAN OI !!!!!!");
+      setLimitPlayer(true);
+    }
+  };
+
+  console.log("list", name);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      {!limitPlayer && (
+        <section id="input-form">
+          <input
+            placeholder="Input Your players"
+            onChange={(e) => onChangeform(e.target.value)}
+            value={name}
+          ></input>
+          <button onClick={() => onSubmitPlayers()}>submit</button>
+        </section>
+      )}
+      <section id="list-players">
+        <h1>list players</h1>
+        <div className="box-list-players">
+          {listPlayers.length === 0
+            ? "Not Players"
+            : listPlayers.map((data, i) => (
+                <div key={i}>
+                  <p>{data.name}</p>
+                  <p>{data.score}</p>
+                </div>
+              ))}
+        </div>
+      </section>
+    </main>
   );
-}
+};
 
 export default App;
